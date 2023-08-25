e3cb88ac0df6d59c1d16cd976a96475b6bb23813
randymluna <142929971+randymluna@users.noreply.github.com>
2023-08-24 20:12:54 -0700

    Update gyu.py

--- gyu.py
+++ gyu.py
@@ -1,1 +1,9 @@
-print("jhrg")
+print("Welcome to the Space Travel Calculator")
+
+distance = float(input("enter the distance to the celestial object in light-years"))
+
+speed = float (input("enter spacecraft speed"))
+
+time = distance / speed
+
+print("It will take", time, "lightyears")
